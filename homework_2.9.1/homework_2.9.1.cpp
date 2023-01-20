ee91540ca115748f0dd9a8d6923d380383b767bb
Stas Mishin <hidrohobbit@yandex.ru>
2023-01-20 22:38:49 +0400

    initial commit

---
+++ homework_2.9.1/homework_2.9.1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	double numerator_;
+	double denominator_;
+	double division_;
+public:
+	bool operator == (Fraction& other)
+	{
+		return this->division_ == other.division_;
+	}
+	bool operator != (Fraction& other)
+	{
+		return !(*this == other);
+	}
+	bool operator > (Fraction& other)
+	{
+		return this->division_ > other.division_;
+	}
+	bool operator < (Fraction& other)
+	{
+		return other > *this;
+	}
+	bool operator >= (Fraction& other)
+	{
+		return !(*this < other);
+	}
+	bool operator <= (Fraction& other)
+	{
+		return !(*this > other);
+	}
+	Fraction(double numerator, double denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+		division_= (this->numerator_ / this->denominator_);
+	}
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6,11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+
+	return 0;
+}
